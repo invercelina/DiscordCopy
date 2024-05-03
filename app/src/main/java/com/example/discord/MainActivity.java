a8e1433c700d39ad4706602ab6977485b569492d
invercelina <invercelina@gmail.com>
2024-05-03 16:46:33 +0900

    Visibility

--- app/src/main/java/com/example/discord/MainActivity.java
+++ app/src/main/java/com/example/discord/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.discord;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -116,6 +117,70 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        TextView tvconfirm = findViewById(R.id.tv_confirm);
+
+        tvconfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tvnotice.getVisibility() == View.GONE) {
+                    tvnotice.setVisibility(View.VISIBLE);
+                } else {
+                    tvnotice.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        TextView tvquestion = findViewById(R.id.tv_question);
+
+        tvquestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tvcoworker.getVisibility() == View.GONE) {
+                    tvcoworker.setVisibility(View.VISIBLE);
+                } else {
+                    tvcoworker.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        TextView tvedu = findViewById(R.id.tv_edu);
+
+        tvedu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tvai.getVisibility() == View.GONE) {
+                    tvai.setVisibility(View.VISIBLE);
+                    tvapp.setVisibility(View.VISIBLE);
+                    tvproject.setVisibility(View.VISIBLE);
+                } else {
+                    tvai.setVisibility(View.GONE);
+                    tvapp.setVisibility(View.GONE);
+                    tvproject.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        TextView tvintimate = findViewById(R.id.tv_intimate);
+        TextView tvstudy = findViewById(R.id.tv_study);
+        ImageView ivvolume = findViewById(R.id.iv_volume);
+        tvintimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tvshare.getVisibility() == View.GONE) {
+                    tvrecommendation.setVisibility(View.VISIBLE);
+                    tvofftherecord.setVisibility(View.VISIBLE);
+                    tvshare.setVisibility(View.VISIBLE);
+                    tvstudy.setVisibility(View.VISIBLE);
+                    ivvolume.setVisibility(View.VISIBLE);
+                } else {
+                    tvrecommendation.setVisibility(View.GONE);
+                    tvofftherecord.setVisibility(View.GONE);
+                    tvshare.setVisibility(View.GONE);
+                    tvstudy.setVisibility(View.GONE);
+                    ivvolume.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
 }
